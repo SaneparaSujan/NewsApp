8fedc8eb99e0ec553412ba6bae105886a9dd7750
SaneparaSujan <159936671+SaneparaSujan@users.noreply.github.com>
2025-11-06 17:51:03 +0530

    Update default image URL in NewsItem component

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,7 +8,7 @@ const NewsItem = ({ title, description, imageurl, newsurl, author, date, source 
           <span className='badge rounded-pill bg-danger'>{source}</span>
         </div>
         <img
-          src={imageurl ? imageurl : "https://image/imageserve/67fed3f128b4d5566865db7c/0x0.jpg?format=jpg&height=900&width=1600&fit=bounds"}
+          src={imageurl ? imageurl : "https://imageio.forbes.com/specials-images/imageserve/67fed3f128b4d5566865db7c/0x0.jpg?format=jpg&height=900&width=1600&fit=bounds"}
           className="card-img-top"
           alt="news"
         />
